dfb5215f506cb27dd57dddfc3a67a13bfa0c310f
Nikolay <nikolaymosyagin94@gmail.com>
2018-12-26 21:53:34 +0200

    Создание главного меню

---
+++ ShipAttack/Assets/Scripts/MainMenuUI.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuUI : MonoBehaviour {
+
+    public void OnExit()
+    {
+        Application.Quit();
+    }
+
+    public void OnStartNewGame()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("new_game");
+    }
+
+    public void OnSettings()
+    {
+
+    }
+}
